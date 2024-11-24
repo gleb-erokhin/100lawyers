f69b20d71be4948f731addb13f7d757fc2613348
gleb-erokhin <gleb.erokhin@gmail.com>
2024-11-24 16:18:23 +0300

    блок ипуты выполнен

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,3 @@
-const swiper = new Swiper(".reviews-slider", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-
 // Mobile nav button
 const navBtn = document.querySelector('.mobile-nav-btn');
 const nav = document.querySelector('.mobile-nav');
