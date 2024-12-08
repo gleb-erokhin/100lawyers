b1dbdb81a5a408b8cad0687982afedbc50358b02
gleb-erokhin <gleb.erokhin@gmail.com>
2024-12-08 12:28:17 +0300

    блок слайдера стилизован под мобилки

--- src/js/index.js
+++ src/js/index.js
@@ -11,10 +11,28 @@ navBtn.onclick = function () {
 
 // swiper slider
 var swiper = new Swiper(".reviews-slider", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    // slidesPerView: 3,
+    // spaceBetween: 30,
     navigation: {
         nextEl: ".slider-button-next",
         prevEl: ".slider-button-prev",
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1199: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
     },
 });
