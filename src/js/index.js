f4499fd3d3157a9b5c4213a61fca3a7ad952c057
gleb-erokhin <gleb.erokhin@gmail.com>
2024-12-09 05:54:11 +0300

    блок с модальным поиском готов. код js работает

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,3 @@
-// Mobile nav button
-const navBtn = document.querySelector('.mobile-nav-btn');
-const nav = document.querySelector('.mobile-nav');
-const overlay = document.querySelector('.mobile-nav-overlay');
-const menuIcon = document.querySelector('.nav-icon');
-
-navBtn.onclick = function () {
-    nav.classList.toggle('mobile-nav--open');
-    overlay.classList.toggle('mobile-nav-overlay--show');
-    menuIcon.classList.toggle('nav-icon--active');
-    document.body.classList.toggle('no-scroll');
-};
-
 /** Функции */
 // функция закрытия бургер меню через ESC
 function closeBurger() {
@@ -27,11 +14,38 @@ function escCloseBtn(event) {
     }
 }
 
+// Mobile nav button
+const navBtn = document.querySelector('.mobile-nav-btn');
+const nav = document.querySelector('.mobile-nav');
+const overlay = document.querySelector('.mobile-nav-overlay');
+const menuIcon = document.querySelector('.nav-icon');
+
+navBtn.onclick = function () {
+    nav.classList.toggle('mobile-nav--open');
+    overlay.classList.toggle('mobile-nav-overlay--show');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+};
+
 /** Закрываем бургер меню по кнопке ESC */
 document.addEventListener('keydown', escCloseBtn);
 
 /** Закрытие модалки по поле затемнению */
 overlay.addEventListener('click', closeBurger);
+
+/* открыть / закрыть Модальный поиск в мобильной версии */
+const modalSearch = document.querySelector('[data-modal-search="modal-search"]')
+const modalSearchBtn = document.querySelector('[data-modal-search="modal-search-btn"]')
+const modalCloseBtn = document.querySelector('[data-modal-search="modal-search-close"]')
+
+modalSearchBtn.addEventListener('click', function() {
+    modalSearch.classList.add('show');
+})
+modalCloseBtn.addEventListener('click', function() {
+    modalSearch.classList.remove('show');
+})
+
+
 
 // swiper slider
 var swiper = new Swiper(".reviews-slider", {
