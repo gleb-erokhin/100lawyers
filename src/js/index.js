4ef60f3083d9885b59631996fec41ab9dee22240
gleb-erokhin <gleb.erokhin@gmail.com>
2024-12-10 23:07:27 +0300

    Исправление ошибок в названии страниц html и scss а так же в стилях. Добавление js кода на открытие модалок в режимах десктопа и бом версии.

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,10 @@ function closeBurger() {
     nav.classList.remove('mobile-nav--open');
     overlay.classList.remove('mobile-nav-overlay--show');
     menuIcon.classList.remove('nav-icon--active');
+}
+
+function closeModal() {
+    modal.classList.remove('modal--open');
     document.body.classList.remove('no-scroll');
 }
 
@@ -54,11 +58,11 @@ modalCloseBtn.addEventListener('click', function () {
 
 
 
-// модальное окно для логона
+// модальное окно для логона - вход / регистрация
 const modalLogon = document.querySelector('[data-modal-logon="modal-logon"]');
 const modalLogonBtns = document.querySelectorAll('[data-modal-logon="logon"]');
 const modalLogonClose = document.querySelector('[data-modal-logon="close"]');
-const modalBody = document.querySelector('.modal-logon__body');
+const modalLogonBody = document.querySelector('.modal-logon__body');
 
 /** работаем над открытием модального окна */
 modalLogonBtns.forEach(function (item) {
@@ -86,6 +90,27 @@ modalLogonClose.addEventListener('click', closeModalLogon);
 modalLogon.addEventListener('click', closeModalLogon);
 
 /** Отключаем поднятие клика чтобы при нажатии внутри модального окна она не закрывалась */
+modalLogonBody.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+
+
+// Модальное окно заказать звонок, учитывает обе формы как в десктоп так и в мобильной версии
+const modalQst = document.querySelectorAll('[data-question="question"]');
+const modal = document.querySelector('.modal');
+const modalBody = document.querySelector('.modal__body');
+const modalClBtn = document.querySelector('.modal__close');
+
+modalQst.forEach(function(item) {
+    item.addEventListener('click', function() {
+        nav.addEventListener('click', closeBurger);
+        modal.classList.add('modal--open');
+        document.body.classList.add('no-scroll');
+    })
+})
+
+modal.addEventListener('click', closeModal);
+modalClBtn.addEventListener('click', closeModal);
 modalBody.addEventListener('click', function (event) {
     event.stopPropagation();
 });
